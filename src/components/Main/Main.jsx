a16b8c6220f13be0d186c17eb5f80e8b881ebd5d
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-12-02 14:53:31 +0100

    add primo esempio di form

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react"
 import Card from '../Card/Card'
 import style from './Main.module.css'
 import posts from '../posts'
 
 export default function Main() {
+
+  const [title, setTitle] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log("Titolo inviato:" + title)
+  }
+
   return (
     <main>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={title} onChange={e => { setTitle(e.target.value) }} placeholder="Inserisci il titolo" />
+        <button>Invia</button>
+      </form>
+
+
       <section className={style.section}>
         <div className="container">
           {
