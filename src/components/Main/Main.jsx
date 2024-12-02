a60b3c2c5d61536b0cc2b43e67bf8ae99fbb8ba5
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-12-02 13:50:09 +0100

    upgrade esercizio dei blog

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,33 @@
+import Card from '../Card/Card'
+import style from './Main.module.css'
+import posts from '../posts'
+
+export default function Main() {
+  return (
+    <main>
+      <section className={style.section}>
+        <div className="container">
+          {
+            posts.length ?
+              <div className="row">
+                {posts.map((post) => (
+                  <div key={post.id} className="col-6" >
+                    <Card
+                      title={post.title}
+                      tags={post.tags}
+                      image={post.image}
+                      published={post.published}
+                    />
+                  </div>
+                ))}
+              </div> :
+              <p>Non ci sono post</p>
+          }
+        </div>
+      </section>
+
+
+    </main>
+
+  )
+}
